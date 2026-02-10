c34ae2e29d08d0c8c7dbd451a05952fc0c14dcb5
Igor Theophilo <igorth.castro@gmail.com>
2026-02-10 18:07:37 -0300

    Door Test Drive 2.0

--- DogDoorSimulator.java
+++ DogDoorSimulator.java
@@ -5,10 +5,16 @@ public class DogDoorSimulator {
         System.out.println("O cachorro esta latindo para sair...");
         remote.pressButton();
         System.out.println("O cachorro saiu...");
-        remote.pressButton();
         System.out.println("O cachorro já terminou...");
+        
+        try {
+            Thread.currentThread().sleep(10000);
+        } catch (InterruptedException e) {}
+
+        System.out.println("O cachorro está preso lá fora!...");
+        System.out.println("O cachorro começou a latir...");
+        System.out.println("Todd pega o controle remoto...");
         remote.pressButton();
         System.out.println("O cachorro voltou para dentro de casa...");
-        remote.pressButton();
     }
 }
